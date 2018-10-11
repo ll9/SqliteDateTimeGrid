82932de98b737439afe011ab81987d0f692415c9
ll9 <llwierer@web.de>
2018-10-11 21:57:11 +0200

    basic adapter fill command

--- SqliteDateTimeGrid/Form1.cs
+++ SqliteDateTimeGrid/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace SqliteDateTimeGrid
         public Form1()
         {
             InitializeComponent();
+
+            var sqliteService = new SqliteService("lds.sqlite", "lds_features");
+            var dataTable = sqliteService.GetDataTable();
+            Debug.WriteLine(dataTable.Columns[0].DataType);
+            dataGridView1.DataSource = sqliteService.GetDataTable();
         }
     }
 }
